38dd9354fb52c542563b5875ae962477ccf130b7
KOBAYASHI_Ryuta <ap3496koba@gmail.com>
2017-04-19 05:29:38 +0000

    modify edit, destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,11 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @micropost
+      redirect_to root_path
+    end
+  end
 end
